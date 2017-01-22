ac84301be0a7703427e7195b06af376920220881
Ahana Ghosh <ahanag22@uw.edu>
2017-01-22 00:00:35 -0800

    Now princess can walk to both the direction

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var AM = new AssetManager();
+var dir = true;
 
 function Animation(spriteSheet, frameWidth, frameHeight, sheetWidth, frameDuration, frames, loop, scale) {
     this.spriteSheet = spriteSheet;
@@ -70,9 +71,26 @@ Princess.prototype.draw = function () {
     this.animation.drawFrame(this.game.clockTick, this.ctx, this.x, this.y);
 }
 
+
 Princess.prototype.update = function () {
-    this.x -= this.game.clockTick * this.speed;
-    if (this.x < 0) this.x = 1000;
+    if (this.x <= 0) {
+		dir = false;
+
+	}
+	if (this.x >= 1000 ) {
+	    dir = true;
+	   
+	}
+	if(dir){
+	    this.x -= this.game.clockTick * this.speed;
+		Entity.prototype.update.call(this);
+	}
+	if(dir == false){
+	    this.x += this.game.clockTick * this.speed;
+		Entity.prototype.update.call(this);
+	}
+	
+  
 }
 
 
@@ -189,3 +207,85 @@ AM.downloadAll(function () {
 
     console.log("All Done!");
 });
+
+/**
+//var canvas;// the canvas element which will draw on
+//var ctx;// the "context" of the canvas that will be used (2D or 3D)function
+Function Animation_jump(){
+var dx = 50;// the rate of change (speed) horizontal object
+var x = 30;// horizontal position of the object (with initial value)
+var y = 150;// vertical position of the object (with initial value)
+var limit = 10; //jump limit
+var jump_y = y;
+var WIDTH = 1000;// width of the rectangular area
+var HEIGHT = 340;// height of the rectangular area
+//var tile1 = new Image ();// Image to be loaded and drawn on canvas
+var posicao = 0;// display the current position of the character
+var NUM_POSICOES = 6;// Number of images that make up the movement
+var goingDown = false;
+var jumping;
+}
+
+function KeyDown(evt){
+    switch (evt.keyCode) {
+        case 39:  /* Arrow to the right 
+            if (x + dx < WIDTH){
+                x += dx;
+                posicao++;
+                if(posicao == NUM_POSICOES)
+                    posicao = 1;
+
+                Update();
+            }
+            break;    
+        case 38:
+            jumping = setInterval(Jump, 100);
+    }
+}
+
+Guy.prototype.draw = function() {      
+    ctx.font="20px Georgia";
+    ctx.beginPath();
+    ctx.fillStyle = "red";   
+    ctx.beginPath();
+    ctx.rect(x, y, 10, 10);
+    ctx.closePath();
+    ctx.fill();   
+    console.log(posicao);
+	}
+function LimparTela() {
+    ctx.fillStyle = "rgb(233,233,233)";   
+    ctx.beginPath();
+    ctx.rect(0, 0, WIDTH, HEIGHT);
+    ctx.closePath();
+    ctx.fill();   
+}
+function Update() {
+    LimparTela();    
+    Draw();
+}
+
+var Jump = function(){
+    if(y > limit && !goingDown){
+        y-=10;
+        console.log('jumping: ' + y);
+    } else{
+    goingDown = true;
+        y +=10;
+        if(y > jump_y){
+            clearInterval(jumping);
+            goingDown = false;
+        }
+
+    }
+}
+
+function Start() {
+    canvas = document.getElementById("gameWorld");
+    ctx = canvas.getContext("2d");
+    return setInterval(Update, 100);
+}
+
+window.addEventListener('keydown', KeyDown);
+Start();
+*/
